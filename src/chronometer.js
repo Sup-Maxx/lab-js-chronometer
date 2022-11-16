b0da77f8123e33bc83705b0182dbb294353b053a
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2022-11-16 15:10:36 +0000

    all lab solved

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,41 +1,47 @@
 class Chronometer {
   constructor() {
-    this.currentTime = 0
-    this.intervalId = null
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(printTimeCallback) {} // didn't get what to do 
-  
+  start(printTimeCallback) {
+    const intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    }, 1000);
+    this.intervalId = intervalId;
+  }
 
   getMinutes() {
-    let minutes = this.currentTime / 60
-    return Math.floor(minutes)
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-    let seconds = this.currentTime % 60
-    return Math.floor(seconds)
+    let seconds = this.currentTime % 60;
+    return Math.floor(seconds);
   }
 
   computeTwoDigitNumber(value) {
-    let string = value.toString()
-    if (string.length === 1) return "0" + string
-    if (string.length === 2) return string
+    let string = value.toString();
+    if (string.length === 1) return "0" + string;
+    if (string.length === 2) return string;
   }
 
   stop() {
-    clearInterval (this.intervalId)
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    this.currentTime= 0  
-}
+    this.currentTime = 0;
+  }
 
   split() {
-    // ... your code goes here
-    let splitMinutes = this.computeTwoDigitNumber(this.getMinutes)
-    let splitSeconds = this.computeTwoDigitNumber(this.getSeconds)
-
-    return `${this.splitMinutes}:${this.splitSeconds}` // not working    
+    let splitMinutes = this.computeTwoDigitNumber(this.getMinutes())
+    let splitSeconds = this.computeTwoDigitNumber(this.getSeconds())
+   
+    return `${splitMinutes}:${splitSeconds}`; 
   }
 }
